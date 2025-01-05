0794cd228a78742d823fa329331fd669c695e395
vikzhunt <vikashsingh@users.github.com>
2025-01-06 00:53:30 +0530

    updated navbar

--- Frontend/src/components/Patient/Dashboard.js
+++ Frontend/src/components/Patient/Dashboard.js
@@ -1,7 +1,6 @@
 import "../../styles/PatientStyle/Dashboard.css"
 import { useState, useEffect } from "react";
 import { Link, useLocation } from 'react-router-dom';
-
 import Profile from "./Profile.js";
 import AllDoctors from "./AllDoctors.js"
 import Bookings from "./Bookings.js"
@@ -10,13 +9,15 @@ import Notification from "./Notifications.js"
 import Ambulance from "./Ambulance.js"
 import HospitalLocator from "./HospitalLocator.js";
 import DiseasePredictor from "./DiseasePrediction.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faUser , faStethoscope, faCalendarAlt, faFileAlt, faBell, faAmbulance, faHospital, faDiagnoses, faSignOutAlt
+} from '@fortawesome/free-solid-svg-icons';
 
 
 const Dashboard = () => {
-
     const location = useLocation();
     const email = localStorage.getItem('email');
-
     useEffect(() => {
         if (email) {
             console.log("Email for this user:", email);
@@ -34,38 +35,30 @@ const Dashboard = () => {
         HospitalLocator: <HospitalLocator />,
         DiseasePredictor: <DiseasePredictor />
     };
-
-
     const [selectedBtn, setBtn] = useState("Profile");
     const btnAction = (pressed) => {
         setBtn(pressed);
     }
-
-
     return (
         <div className="DashboardContainer">
-            <div className="navigation">
-                <button className="naviBtn" onClick={() => btnAction("AllDoctors")}>All Doctors</button>
-                <button className="naviBtn" onClick={() => btnAction("Bookings")}>My Bookings</button>
-                <button className="naviBtn" onClick={() => btnAction("Documents")}>Documents</button>
-                <button className="naviBtn" onClick={() => btnAction("Notification")}>Notification</button>
-                <button className="naviBtn" onClick={() => btnAction("Ambulance")}>Ambulance Service</button>
-                <button className="naviBtn" onClick={() => btnAction("HospitalLocator")}>Nearby Hospitals</button>
-                <button className="naviBtn" onClick={() => btnAction("DiseasePredictor")}>Disease Analyzier</button>
-                <Link to="/"><button className="naviBtn" >Logout</button></Link>
-
-
+            <div className="navigation" >
+                <h1 className="text-3xl text-slate-100 border-2 py-2 mb-2 rounded border-slate-500 text-center hover:border-white">Medi-Care</h1>
+                <button className="naviBtn" onClick={() => btnAction("AllDoctors")}><FontAwesomeIcon className="pr-2" icon={faStethoscope} />All Doctors</button>
+                <button className="naviBtn" onClick={() => btnAction("Bookings")}><FontAwesomeIcon className="pr-2" icon={faCalendarAlt} />My Bookings</button>
+                <button className="naviBtn" onClick={() => btnAction("Documents")}><FontAwesomeIcon className="pr-2" icon={faFileAlt} />Documents</button>
+                <button className="naviBtn" onClick={() => btnAction("Notification")}><FontAwesomeIcon className="pr-2" icon={faBell} />Notification</button>
+                <button className="naviBtn" onClick={() => btnAction("Ambulance")}><FontAwesomeIcon className="pr-2" icon={faAmbulance} />Ambulance Service</button>
+                <button className="naviBtn" onClick={() => btnAction("HospitalLocator")}><FontAwesomeIcon className="pr-2" icon={faHospital} />Nearby Hospitals</button>
+                <button className="naviBtn" onClick={() => btnAction("DiseasePredictor")}><FontAwesomeIcon className="pr-2" icon={faDiagnoses} />Disease Analyzier</button>
+                <Link to="/"><button className="naviBtn logout" ><FontAwesomeIcon className="pr-2" icon={faSignOutAlt} />Logout</button></Link>
             </div>
-
             <div className="RHS">
                 <div className="profileNavi">
-                    <button className="profile" onClick={() => btnAction("Profile")}>Profile</button>
+                    <button className="profile" onClick={() => btnAction("Profile")}><FontAwesomeIcon className="pr-2" icon={faUser } />Profile</button>
                 </div>
-
                 <div className="details">
                     {selectedBtnMap[selectedBtn]}
                 </div>
-
             </div>
         </div>
     )
